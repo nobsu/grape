daffd05107c798d2131126322aaba78f36fa7ab9
sudongdong <sudongdong@ppzuche.com>
2015-07-11 11:00:25 +0800

    d

--- grape/utils.py
+++ grape/utils.py
@@ -4,8 +4,20 @@ Created on 2015年7月10日
 
 @author: nob
 '''
+import hashlib
+
 def md5(str):
-    import hashlib
     m = hashlib.md5()   
     m.update(str)
     return m.hexdigest()
+
+def md5hex(word):  
+     """ MD5加密算法，返回32位小写16进制符号 
+     """   
+     if isinstance(word, unicode):  
+         word = word.encode("utf-8")  
+     elif not isinstance(word, str):  
+         word = str(word)  
+     m = hashlib.md5()  
+     m.update(word)  
+     return m.hexdigest() 
